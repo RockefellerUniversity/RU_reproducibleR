2859bec734cbf3a0322cb7aa842ec36202eb8f72
matthew-paul-2006 <matthew.paul.2006@gmail.com>
2020-07-30 11:36:43 -0400

    update rmarkdown pathways

--- r_course/scripts/scriptToConvertToRMarkdown.r
+++ r_course/scripts/scriptToConvertToRMarkdown.r
@@ -1,4 +1,6 @@
-filesToRead <- dir("../ExpressionResults/",pattern = "*\\.txt",full.names=T)
+filesToRead <- dir("data/ExpressionResults/",pattern = "*\\.txt",full.names=T)
+filesToRead <- filesToRead[-grep('Anno',filesToRead)]
+
 fileRead <- vector("list",length=length(filesToRead))
 for(i in 1:length(filesToRead)){
   fileRead[[i]] <- read.delim(filesToRead[i],header=F,sep="\t")
@@ -17,7 +19,7 @@ for(i in fileRead){
 
 mergedTable[1:3,] 
 
-Annotation <- read.table("../ExpressionResults/Annotation.ann",sep="\t",h=T)
+Annotation <- read.table("data/ExpressionResults/Annotation.txt",sep="\t",h=T)
 annotatedExpression <- merge(Annotation,mergedTable,by=1,all.x=F,all.y=T)
 annotatedExpression[1:2,]
 
@@ -41,3 +43,4 @@ colnames(testResult) <- c("logFC","tStatistic","pValue")
 annotatedResult <- cbind(annotatedExpression[,1:3],testResult)
 annotatedResult <- annotatedResult[order(annotatedResult$tStatistic),]
 annotatedResult[1:2,]
+
